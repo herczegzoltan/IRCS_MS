ef0c21f1e72f51caae54679fc5753b03e1f4473b
Zoltan Herczeg <zoltan.herczeg>
2020-04-29 00:21:15 +0200

    Add byte messages

--- IRCS_MS/IRCS_MS/Infrastructure/ByteMessages.cs
+++ IRCS_MS/IRCS_MS/Infrastructure/ByteMessages.cs
@@ -6,15 +6,15 @@ using System.Threading.Tasks;
 
 namespace IRCS_MS.Infrastructure
 {
-    public class ByteMessages
+    public sealed class ByteMessages
     {
         public byte[] MeasureModeIncoming = new byte[3] { 0x00, 0x00, 0x00 };
 
         public byte[] MeasureModeOutgoing = new byte[5] { 0x00, 0x00, 0x00, 0x00, 0x00 };
 
-        public byte[] ServiceModeIncoming = new byte[5] { 0x00, 0x00, 0x00, 0x00, 0x00 };
+        public byte[] ServiceModeIncoming = new byte[6] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 
-        public byte[] ServiceModeOutgoing = new byte[5] { 0x00, 0x00, 0x00, 0x00, 0x00 };
+        public byte[] ServiceModeOutgoing = new byte[12] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
         private static ByteMessages instance;
         private ByteMessages()
